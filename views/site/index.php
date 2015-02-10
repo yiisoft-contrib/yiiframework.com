18f0eb5bf9bedab25c7cf6f03768e9b31f6e6031
Alexander Makarov <sam@rmcreative.ru>
2015-02-11 02:21:00 +0300

    Better scaling of the front page on various screens

--- views/site/index.php
+++ views/site/index.php
@@ -5,29 +5,29 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="row">
-        <div class="col-xs-4 col-xs-offset-4 text-center">
-            <h1>PHP. Less noise. More signal.</h1>
+        <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 text-center">
+            <h1>PHP. Less noise.<br/>More signal.</h1>
 
             <a href="#getting-started" class="btn btn-danger">You gonna <span class="glyphicon glyphicon-heart" aria-hidden="true"></span> it!</a>
         </div>
     </div>
 
     <div class="row testimonials">
-        <div class="col-xs-3">
+        <div class="col-xs-12 col-sm-6 col-md-3">
             <blockquote>
                 <p>I do want to say we are thoroughly impressed with Yii 2.</p>
                 <footer><cite><a href="http://buildwithcraft.com/">Brandon Anonymous, Craft</a></cite></footer>
             </blockquote>
         </div>
 
-        <div class="col-xs-3 col-xs-offset-1">
+        <div class="col-xs-12 col-sm-6 col-md-3 col-md-offset-1">
             <blockquote>
                 <p>We're using it for years. Yii is the right tool for the job.</p>
                 <footer><cite><a href="http://www.stay.com/">Asgeir Anonymous, Stay.com</a></cite></footer>
             </blockquote>
         </div>
 
-        <div class="col-xs-3 col-xs-offset-1">
+        <div class="col-xs-12 col-md-3 col-md-offset-1">
             <blockquote>
                 <p>It powers our business. Definitely worth learning!</p>
                 <footer><cite><a href="#">Anonymous, anonymous</a></cite></footer>
@@ -36,7 +36,7 @@ $this->title = 'My Yii Application';
     </div>
 
     <div class="row promises">
-        <div class="col-xs-4 col-xs-offset-4">
+        <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
             <ul>
                 <li><span class="glyphicon glyphicon-time" aria-hidden="true"></span> Saves your time.</li>
                 <li><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Solves PHP quirks for you.</li>
@@ -47,13 +47,13 @@ $this->title = 'My Yii Application';
 
     <div class="features">
         <div class="row">
-            <div class="col-xs-4 col-xs-offset-4 text-center">
+            <div class="col-xs-12 col-sm-6 col-sm-offset-3 text-center">
                 <h3>Made by professionals</h3>
             </div>
         </div>
 
         <div class="row">
-            <div class="col-xs-2 text-center col-xs-offset-3">
+            <div class="col-xs-2 text-center col-xs-offset-3 col-sm-offset-3">
                 <h4><span class="glyphicon glyphicon-gift" aria-hidden="true"></span> Rich</h4>
             </div>
 
@@ -67,7 +67,7 @@ $this->title = 'My Yii Application';
         </div>
 
         <div class="row">
-            <div class="col-xs-4 col-xs-offset-4 text-center">
+            <div class="col-xs-12 col-sm-6 col-sm-offset-3 text-center">
                 <p>We're creating frameworks since 2004.</p>
             </div>
         </div>
@@ -75,7 +75,7 @@ $this->title = 'My Yii Application';
 
 
     <div class="row">
-        <div class="col-xs-4 col-xs-offset-4 text-center">
+        <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 text-center">
             <a href="#getting-started" class="btn btn-danger">Learn how to develop faster with Yii</a>
         </div>
     </div>
