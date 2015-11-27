3c183be6f429b6022420109cba593cb3b1e1af18
Jacob Moen <jacmoe.dk@gmail.com>
2015-11-27 19:26:35 +0100

    Features responsive work - for desktop and tablet - hidden on mobile

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ JUMBOTRON - START
     <div class="container">
 		<h1><img src="<?= Yii::getAlias('@web/image/yii_petals.svg') ?>" alt="Yii Framework" width="80" />yii<span class="hero-framework">framework</span></h1>
         <h2>The solid foundation for your PHP application.</h2>
-        <div class="row features">
+        <div class="row features visible-md visible-lg">
             <div class="col-md-10 col-md-offset-2">
                 <ul class="fa-ul">
                     <li><i class="fa-li fa fa-rocket fa-inverse fa-2x"></i><strong>Fast</strong>  Yii gives you maximum functionality by adding the least possible overhead.</li>
@@ -17,6 +17,52 @@ JUMBOTRON - START
                     <li><i class="fa-li fa fa-clock-o fa-inverse fa-2x"></i><strong>Efficient</strong>  Write more code in less time with simple yet powerful APIs and code generation.</li>
                     <li><i class="fa-li fa fa-asterisk fa-inverse fa-2x"></i><strong>Flexible</strong>  It works out of the box using reasonable defaults but is highly adjustable to fit your needs.</li>
                 </ul>
+            </div>
+        </div>
+        <div class="row features-small visible-sm">
+            <div class="col-sm-3">
+                <div class="icon">
+                    <i class="fa fa-rocket fa-inverse fa-2x"></i>
+                </div>
+                <div class="title">
+                    Fast
+                </div>
+                <div class="description">
+                    Yii gives you maximum functionality by adding the least possible overhead.
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="icon">
+                    <i class="fa fa-lock fa-inverse fa-2x"></i>
+                </div>
+                <div class="title">
+                    Secure
+                </div>
+                <div class="description">
+                    Sane defaults and built in tools help you write solid and secure code.
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="icon">
+                    <i class="fa fa-clock-o fa-inverse fa-2x"></i>
+                </div>
+                <div class="title">
+                    Efficient
+                </div>
+                <div class="description">
+                    Write more code in less time with simple yet powerful APIs and code generation.
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="icon">
+                    <i class="fa fa-asterisk fa-inverse fa-2x"></i>
+                </div>
+                <div class="title">
+                    Flexible
+                </div>
+                <div class="description">
+                    It works out of the box using reasonable defaults but is highly adjustable to fit your needs.
+                </div>
             </div>
         </div>
     </div>
