5f8f30d3a1c715e34c0651d3840dee95e2fa489e
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-02 15:20:01 +0100

    Highlight widget

---
+++ components/Highlight.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace app\components;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+/**
+ * Highlight widget renders the content between it's begin end tags.
+ *
+ * Usage example:
+ *
+ * ```php
+ * <body>
+ *     <?php Highlight::begin(); ?>
+ *         <div class="nav-bar">
+ *         </div>
+ *         <div class="content">
+ *         </div>
+ *     <?php Highlight::end(); ?>
+ * </body>
+ * ```
+ */
+class Highlight extends Widget
+{
+    public $language = 'php';
+
+    protected $highlighter = null;
+
+    /**
+     * Starts capturing an output to be highlighted.
+     */
+    public function init()
+    {
+        parent::init();
+        ob_start();
+    }
+
+    protected function getHighlighter()
+    {
+        if(!isset($this->highlighter))
+        {
+            $this->highlighter = new \Highlight\Highlighter();
+            return $this->highlighter;
+        } else {
+            return $this->highlighter;
+        }
+    }
+
+    /**
+     * Marks the end of content to be highlighted.
+     * Stops capturing an output and echoes highlighted result.
+     */
+    public function run()
+    {
+        $content = ob_get_clean();
+        $highlighter = $this->getHighlighter();
+        $result = $highlighter->highlight($this->language, Html::encode($content));
+        return "<pre class='hljs " . $result->language . "'>" . $result->value . "</pre>";
+    }
+}
--- views/site/_quickstart.php
+++ views/site/_quickstart.php
@@ -1,6 +1,6 @@
 <?php
 use yii\helpers\Html;
-$hl = new Highlight\Highlighter();
+use app\components\Highlight as HL;
 ?>
 <!-- start of quickstart -->
 <section class="section-quickstart">
@@ -14,8 +14,8 @@ $hl = new Highlight\Highlighter();
             <p>
                 Creating a project with Yii can be done in less than 5 minutes by creating a project using Composer and a project template:
             </p>
-            <?php $r = $hl->highlight("bash", Html::encode('composer create-project --prefer-dist yiisoft/yii2-app-basic basic')); ?>
-            <pre class="hljs <?=$r->language?>"><?=$r->value?></pre>
+            <?php HL::begin(['language'=>'bash']); ?>
+composer create-project --prefer-dist yiisoft/yii2-app-basic basic<?php HL::end(); ?>
             <p>
                 For more information about how to get started with Yii quickly
             </p>
