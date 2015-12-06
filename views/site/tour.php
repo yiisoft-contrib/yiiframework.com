c1d1d190782cc5855f1d7140e29d825ad8bef2bd
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-06 19:48:56 +0100

    Getting started: lines adjusted

--- views/site/tour.php
+++ views/site/tour.php
@@ -206,6 +206,15 @@ class m150416_155923_create_comment_table extends Migration
                             <?= $hl->captured; ?>
 
                         <?php Modal::end(); ?>
+                        <p class="text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <p class="text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
+                        <p class="text-muted">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </p>
                     </div>
                 </div>
             </div>
