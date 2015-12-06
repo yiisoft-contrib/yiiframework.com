cf9865db9b8495be15baf7497382630ed8f8b17b
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-06 15:33:33 +0100

    Got rid of unused fonts

--- Gulpfile.js
+++ Gulpfile.js
@@ -68,7 +68,7 @@ gulp.task('images', function() {
 
 // Copy fonts
 gulp.task('fonts', function() {
-  gulp.src(['vendor/bower/bootstrap/fonts/*','scss/2-vendors/fontawesome/fonts/*', 'scss/2-vendors/fonts/josefine-sans/*'])
+  gulp.src(['vendor/bower/bootstrap/fonts/*'])
   .pipe(gulp.dest('./web/fonts'));
 });
 
