7f7e7a5e1099b05c3548cbf7a72ce1ee5b8c9020
Robert Korulczyk <robert@korulczyk.pl>
2015-12-12 15:28:14 +0100

    move font-awesome to bower

--- Gulpfile.js
+++ Gulpfile.js
@@ -68,7 +68,7 @@ gulp.task('images', function() {
 
 // Copy fonts
 gulp.task('fonts', function() {
-  gulp.src(['vendor/bower/bootstrap/fonts/*','scss/2-vendors/fontawesome/fonts/*'])
+  gulp.src(['vendor/bower/bootstrap/fonts/*','vendor/bower/font-awesome/fonts/*'])
   .pipe(gulp.dest('./web/fonts'));
 });
 
