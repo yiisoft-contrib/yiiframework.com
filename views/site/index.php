908da83ec86b75f11b087d9d5f0fb7ec7ac771fe
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-15 03:52:02 +0100

    Clean slate

--- views/site/index.php
+++ views/site/index.php
@@ -2,36 +2,4 @@
 /* @var $this yii\web\View */
 ?>
 <div class="site-index">
-<!-- ==========================
-JUMBOTRON - START
-=========================== -->
-<div class="jumbotron jumbotron-index">
-    <div class="container">
-		<h1>
-            <span class="hidden-xs">
-                <img src="<?= Yii::getAlias('@web/image/logo-light-80.png') ?>" alt="Yii Framework" />
-            </span>
-            <span class="visible-xs">
-                <img src="<?= Yii::getAlias('@web/image/logo-light-42.png') ?>" alt="Yii Logo">
-            </span>
-        </h1>
-        <h2>The solid foundation for your PHP application.</h2>
-
-        <?= $this->render('_jumbofeatures'); ?>
-
-    </div>
-</div>
-
-    <?php echo $this->render('_features'); ?>
-
-    <?= $this->render('_quickstart'); ?>
-
-    <?= $this->render('_recentnews'); ?>
-
-    <?= $this->render('_bookscommunity'); ?>
-
-    <?= $this->render('_poweredby'); ?>
-
-    <?php //echo $this->render('_testimonials'); ?>
-
 </div> <!-- class site-index -->
