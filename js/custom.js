ab5a12c953116986550f29448356fea536230109
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-15 20:59:55 +0100

    ScrollUp removed

--- js/custom.js
+++ js/custom.js
@@ -7,39 +7,4 @@ $(document).ready(function() {
 		$('html, body').animate({ scrollTop: $(this.hash).offset().top }, 250, function(){window.location.hash = hash;});
 	});
 
-	$(function () {
-      $.scrollUp({
-          scrollName: 'scrollUp', // Element ID
-          scrollDistance: 140, // Distance from top/bottom before showing element (px)
-          scrollFrom: 'top', // 'top' or 'bottom'
-          scrollSpeed: 300, // Speed back to top (ms)
-          easingType: 'linear', // Scroll to top easing (see http://easings.net/)
-          animation: 'fade', // Fade, slide, none
-          animationInSpeed: 200, // Animation in speed (ms)
-          animationOutSpeed: 200, // Animation out speed (ms)
-          scrollText: 'Scroll to top', // Text for element, can contain HTML
-          scrollTitle: false, // Set a custom <a> title if required. Defaults to scrollText
-          scrollImg: true, // Set true to use image
-          activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
-          zIndex: 2147483647 // Z-Index for the overlay
-      });
-  });
-
-  $('#quote-carousel').carousel({
-    pause: true,
-    interval: false,
-  });
-
-	$(function () {
-	  $(".equalizer").each(function() {
-	    var heights = $(this).find(".watch").map(function() {
-	      return $(this).height();
-	    }).get(),
-
-	    maxHeight = Math.max.apply(null, heights);
-
-	    $(".watch").height(maxHeight);
-	  });
-	});
-
 });
