02ae08a72249ae899e93e0961e0f546e9ea8df02
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-18 22:28:14 +0100

    Front jumbo background image fixes

--- views/site/partials/index/_jumbo.php
+++ views/site/partials/index/_jumbo.php
@@ -3,7 +3,7 @@
         <div class="row">
             <h1>The solid foundation for your PHP application</h1>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                Pinterest everyday carry cold-pressed literally PBR&B, chicharrones neutra heirloom keytar pop-up. Pabst deep v cold-pressed celiac, twee food truck cardigan. Cray kickstarter lumbersexual, tattooed meditation XOXO 90's. Ramps beard viral keffiyeh listicle, chambray distillery. Franzen banjo chartreuse iPhone gentrify hashtag. Kogi trust fund flexitarian, synth forage squid scenester tacos. Hammock try-hard master cleanse yuccie, asymmetrical gastropub seitan.
             </p>
         </div>
     </div>
