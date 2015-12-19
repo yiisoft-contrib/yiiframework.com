ae864c332333a204f969de622475e0849d4acfab
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-19 01:42:07 +0100

    Wiki: blur and parallax works fine :)

--- views/site/wiki.php
+++ views/site/wiki.php
@@ -1,76 +1,84 @@
 <?php
 $this->registerJs("
-    function fader() {
-      var r = $('.blurred'),
-        wh = $(window).height(),
-        dt = $(document).scrollTop(),
-        elView, opacity;
+function fader() {
+    var r = $('.blurred'),
+    wh = $(window).height(),
+    dt = $(document).scrollTop(),
+    elView, opacity;
 
-      // Loop elements with class 'blurred'
-      r.each(function() {
-        elView = wh - ($(this).offset().top - dt + ($('.wikijumbo').height()*2));
+    // Loop elements with class 'blurred'
+    r.each(function() {
+        elView  = wh - ($(this).offset().top - dt + 300);
         if (elView > 0) { // Top of DIV above bottom of window.
-          opacity = 1 / (wh + $(this).height()) * elView * 8
-          if (opacity < 1) // Bottom of DIV below top of window.
+            opacity = 1 / (wh + $(this).height()) * elView * 3
+            if (opacity < 1) // Bottom of DIV below top of window.
             $(this).css('opacity', opacity);
         }
-      });
-    }
 
-    $(document).bind('scroll', fader);
+    });
+}
+$(document).bind('scroll', fader);
 
-    var jumboHeight = $('.image-wrap').height();
-    function parallax(){
-        var scrolled = $(window).scrollTop();
-        $('.image-wrap').css('height', (jumboHeight-scrolled) + 'px');
-    }
-    $(document).bind('scroll', parallax);
 
+(function() {
+    $(window).scroll(function() {
+        var oVal;
+        oVal = $(window).scrollTop() / 240;
+        return $('.blur').css('opacity', oVal);
+    });
+
+}).call(this);
 ");
 ?>
-<div class="wikijumbo">
-    <div class="image-wrap">
-      <div class="regular" style="background:url(./image/yiicoding.jpg) no-repeat center center; background-size: cover;"></div>
-      <div class="blurred" style="background:url(./image/yiicoding-blurred.jpg) no-repeat center center; background-size: cover;"></div>
-    </div>
-    <div class="wiki-header">
-        <h1>Yii Wiki</h1>
+<div class='blurImg'>
+    <div style="background-image: url('./image/yiicoding.jpg')"></div>
+    <div class='blur' style="background-image: url('./image/yiicoding-blurred.jpg')"></div>
+</div>
+<div class="wiki-header">
+    <div>
+        <h1>
+            Yii Wiki
+        </h1>
         <p>
-            Tutorials and Snippets
+            Tutorials and code snippets
         </p>
+    </div>
+    <div class="wiki-nav">
     </div>
 </div>
-<div class="container">
-    <div class="row content">
-        <p>
-            Meh listicle marfa, VHS XOXO messenger bag etsy. Post-ironic mumblecore poutine occupy, mixtape neutra everyday carry polaroid ethical biodiesel shabby chic listicle craft beer. Bicycle rights wayfarers kogi photo booth portland listicle. Meh YOLO echo park, godard fanny pack single-origin coffee next level. Semiotics four dollar toast shoreditch raw denim, +1 tumblr pour-over. Kitsch aesthetic four dollar toast kombucha, mumblecore meggings yuccie selvage deep v hashtag marfa small batch DIY green juice narwhal. Pop-up vinyl trust fund keffiyeh deep v, skateboard shabby chic normcore stumptown.
-        </p>
-        <p>
-            Keytar authentic 90's, ethical poutine butcher celiac. Leggings vegan shabby chic, chartreuse yuccie flexitarian marfa. Tilde bushwick kitsch fanny pack tousled 3 wolf moon, flexitarian forage schlitz. Squid lo-fi venmo, try-hard four dollar toast umami sartorial occupy seitan quinoa biodiesel master cleanse. Lomo post-ironic cray, cold-pressed 90's narwhal small batch single-origin coffee migas freegan +1 pour-over roof party normcore brooklyn. Wolf PBR&B chillwave literally, everyday carry aesthetic asymmetrical shoreditch. Gastropub roof party fashion axe, listicle fixie lo-fi put a bird on it freegan neutra wayfarers bitters kogi.
-        </p>
-        <p>
-            Etsy DIY mixtape, affogato photo booth taxidermy four loko kombucha kitsch lomo meditation twee skateboard migas post-ironic. Twee iPhone cronut 90's, humblebrag selvage chia microdosing kinfolk gentrify slow-carb bushwick health goth tousled mixtape. Microdosing iPhone gluten-free yuccie four loko quinoa. Fap bespoke readymade next level, tofu normcore mumblecore ramps selvage disrupt hella actually truffaut organic whatever. Food truck direct trade trust fund pug. Hammock iPhone microdosing, put a bird on it shoreditch brooklyn vice wolf messenger bag dreamcatcher meggings narwhal. Etsy fashion axe tattooed four loko, authentic keytar plaid 3 wolf moon schlitz salvia art party yr chia pug next level.
-        </p>
-        <p>
-            Brunch tumblr letterpress squid marfa. Craft beer cornhole helvetica, leggings drinking vinegar cred street art gentrify pour-over actually portland disrupt migas tote bag. +1 umami street art, cronut jean shorts schlitz fingerstache thundercats disrupt. Listicle tumblr bitters, drinking vinegar migas cliche synth pickled artisan microdosing chicharrones. Kombucha chambray pitchfork, fap pinterest cornhole pour-over lumbersexual +1 thundercats normcore you probably haven't heard of them. Slow-carb pitchfork austin, bushwick organic VHS lo-fi cornhole cray ramps asymmetrical ennui cardigan mumblecore. Heirloom normcore gastropub slow-carb, fixie yr brunch tote bag meh helvetica polaroid pabst.
-        </p>
-        <p>
-            Asymmetrical migas humblebrag, pug PBR&B heirloom bespoke pickled whatever hammock brooklyn pitchfork direct trade godard bicycle rights. Authentic farm-to-table quinoa, semiotics fingerstache mlkshk food truck stumptown blue bottle schlitz organic shabby chic butcher. Vinyl yuccie williamsburg, meh fap selfies iPhone ennui. Brooklyn whatever actually, deep v crucifix bushwick messenger bag. Squid keytar pour-over williamsburg, bitters pickled pug. Paleo authentic ethical, mumblecore tacos keffiyeh sriracha raw denim hella godard craft beer you probably haven't heard of them beard pug. Gentrify jean shorts authentic, waistcoat cliche pinterest fingerstache cold-pressed synth meditation helvetica celiac wolf.
-        </p>
-        <p>
-            Bacon ipsum dolor amet short loin pastrami ex kielbasa corned beef. Anim rump consectetur, dolore boudin ex ut aute capicola in id ut. Ham filet mignon picanha, deserunt pastrami drumstick duis boudin ham hock swine. Drumstick shoulder in, pariatur esse tri-tip leberkas sed. Doner id ut eiusmod non tongue in bresaola. Sunt ut chuck tri-tip picanha aliquip et anim tongue jerky quis voluptate drumstick capicola.
-        </p>
-        <p>
-            Beef ribs dolor pig veniam short loin, tenderloin lorem nulla doner. Eiusmod dolor ground round flank cupidatat pariatur, cillum spare ribs short ribs sunt ham in picanha quis. Nostrud frankfurter capicola incididunt lorem tenderloin. Spare ribs ea swine, tenderloin cillum strip steak dolore doner in est turducken fatback pork loin porchetta pig. Fatback andouille sint salami, bresaola duis et elit meatloaf chuck porchetta. Fugiat strip steak qui labore meatball. Sausage turkey culpa irure id jowl occaecat eu venison cupim hamburger non dolor ut.
-        </p>
-        <p>
-            Dolore pastrami tri-tip officia. Ham hock jowl aliqua pork, chicken consectetur cow nisi alcatra shankle short ribs. Short ribs cupidatat sausage, sunt do in proident salami drumstick. Pork drumstick id sed, cupidatat swine deserunt elit in kielbasa pork loin adipisicing cupim shankle tenderloin. Tri-tip shankle tempor, proident sed excepteur kielbasa meatloaf veniam officia eu turkey. Veniam ham hock nulla leberkas est duis ex incididunt in turkey tri-tip tempor swine.
-        </p>
-        <p>
-            Qui sed proident, ham hamburger ad tempor eu kielbasa. Aute tempor kevin cow leberkas. Dolore culpa pariatur short ribs chuck shankle dolore. Lorem mollit chuck deserunt jowl brisket et reprehenderit flank ground round. Fugiat nostrud corned beef laborum ground round. Boudin short ribs rump, cupim pig corned beef shank meatloaf. Dolore sausage minim sint tongue laboris.
-        </p>
-        <p>
-            Shankle cow ham hock eiusmod enim pancetta magna meatloaf et, filet mignon nisi excepteur. Nostrud duis venison shankle beef ribs aliqua ut pariatur drumstick consectetur quis in. Swine anim ut ribeye jerky bresaola. Pork loin enim aute, corned beef pig prosciutto quis consequat. Tongue ham hock ball tip in ut laborum. Laboris et deserunt pork, corned beef non shoulder leberkas hamburger laborum ground round biltong.
-        </p>
+<div class="wiki-container">
+    <div class="container">
+        <div class="row">
+            <p>
+                Meh listicle marfa, VHS XOXO messenger bag etsy. Post-ironic mumblecore poutine occupy, mixtape neutra everyday carry polaroid ethical biodiesel shabby chic listicle craft beer. Bicycle rights wayfarers kogi photo booth portland listicle. Meh YOLO echo park, godard fanny pack single-origin coffee next level. Semiotics four dollar toast shoreditch raw denim, +1 tumblr pour-over. Kitsch aesthetic four dollar toast kombucha, mumblecore meggings yuccie selvage deep v hashtag marfa small batch DIY green juice narwhal. Pop-up vinyl trust fund keffiyeh deep v, skateboard shabby chic normcore stumptown.
+            </p>
+            <p>
+                Keytar authentic 90's, ethical poutine butcher celiac. Leggings vegan shabby chic, chartreuse yuccie flexitarian marfa. Tilde bushwick kitsch fanny pack tousled 3 wolf moon, flexitarian forage schlitz. Squid lo-fi venmo, try-hard four dollar toast umami sartorial occupy seitan quinoa biodiesel master cleanse. Lomo post-ironic cray, cold-pressed 90's narwhal small batch single-origin coffee migas freegan +1 pour-over roof party normcore brooklyn. Wolf PBR&B chillwave literally, everyday carry aesthetic asymmetrical shoreditch. Gastropub roof party fashion axe, listicle fixie lo-fi put a bird on it freegan neutra wayfarers bitters kogi.
+            </p>
+            <p>
+                Etsy DIY mixtape, affogato photo booth taxidermy four loko kombucha kitsch lomo meditation twee skateboard migas post-ironic. Twee iPhone cronut 90's, humblebrag selvage chia microdosing kinfolk gentrify slow-carb bushwick health goth tousled mixtape. Microdosing iPhone gluten-free yuccie four loko quinoa. Fap bespoke readymade next level, tofu normcore mumblecore ramps selvage disrupt hella actually truffaut organic whatever. Food truck direct trade trust fund pug. Hammock iPhone microdosing, put a bird on it shoreditch brooklyn vice wolf messenger bag dreamcatcher meggings narwhal. Etsy fashion axe tattooed four loko, authentic keytar plaid 3 wolf moon schlitz salvia art party yr chia pug next level.
+            </p>
+            <p>
+                Brunch tumblr letterpress squid marfa. Craft beer cornhole helvetica, leggings drinking vinegar cred street art gentrify pour-over actually portland disrupt migas tote bag. +1 umami street art, cronut jean shorts schlitz fingerstache thundercats disrupt. Listicle tumblr bitters, drinking vinegar migas cliche synth pickled artisan microdosing chicharrones. Kombucha chambray pitchfork, fap pinterest cornhole pour-over lumbersexual +1 thundercats normcore you probably haven't heard of them. Slow-carb pitchfork austin, bushwick organic VHS lo-fi cornhole cray ramps asymmetrical ennui cardigan mumblecore. Heirloom normcore gastropub slow-carb, fixie yr brunch tote bag meh helvetica polaroid pabst.
+            </p>
+            <p>
+                Asymmetrical migas humblebrag, pug PBR&B heirloom bespoke pickled whatever hammock brooklyn pitchfork direct trade godard bicycle rights. Authentic farm-to-table quinoa, semiotics fingerstache mlkshk food truck stumptown blue bottle schlitz organic shabby chic butcher. Vinyl yuccie williamsburg, meh fap selfies iPhone ennui. Brooklyn whatever actually, deep v crucifix bushwick messenger bag. Squid keytar pour-over williamsburg, bitters pickled pug. Paleo authentic ethical, mumblecore tacos keffiyeh sriracha raw denim hella godard craft beer you probably haven't heard of them beard pug. Gentrify jean shorts authentic, waistcoat cliche pinterest fingerstache cold-pressed synth meditation helvetica celiac wolf.
+            </p>
+            <p>
+                Bacon ipsum dolor amet short loin pastrami ex kielbasa corned beef. Anim rump consectetur, dolore boudin ex ut aute capicola in id ut. Ham filet mignon picanha, deserunt pastrami drumstick duis boudin ham hock swine. Drumstick shoulder in, pariatur esse tri-tip leberkas sed. Doner id ut eiusmod non tongue in bresaola. Sunt ut chuck tri-tip picanha aliquip et anim tongue jerky quis voluptate drumstick capicola.
+            </p>
+            <p>
+                Beef ribs dolor pig veniam short loin, tenderloin lorem nulla doner. Eiusmod dolor ground round flank cupidatat pariatur, cillum spare ribs short ribs sunt ham in picanha quis. Nostrud frankfurter capicola incididunt lorem tenderloin. Spare ribs ea swine, tenderloin cillum strip steak dolore doner in est turducken fatback pork loin porchetta pig. Fatback andouille sint salami, bresaola duis et elit meatloaf chuck porchetta. Fugiat strip steak qui labore meatball. Sausage turkey culpa irure id jowl occaecat eu venison cupim hamburger non dolor ut.
+            </p>
+            <p>
+                Dolore pastrami tri-tip officia. Ham hock jowl aliqua pork, chicken consectetur cow nisi alcatra shankle short ribs. Short ribs cupidatat sausage, sunt do in proident salami drumstick. Pork drumstick id sed, cupidatat swine deserunt elit in kielbasa pork loin adipisicing cupim shankle tenderloin. Tri-tip shankle tempor, proident sed excepteur kielbasa meatloaf veniam officia eu turkey. Veniam ham hock nulla leberkas est duis ex incididunt in turkey tri-tip tempor swine.
+            </p>
+            <p>
+                Qui sed proident, ham hamburger ad tempor eu kielbasa. Aute tempor kevin cow leberkas. Dolore culpa pariatur short ribs chuck shankle dolore. Lorem mollit chuck deserunt jowl brisket et reprehenderit flank ground round. Fugiat nostrud corned beef laborum ground round. Boudin short ribs rump, cupim pig corned beef shank meatloaf. Dolore sausage minim sint tongue laboris.
+            </p>
+            <p>
+                Shankle cow ham hock eiusmod enim pancetta magna meatloaf et, filet mignon nisi excepteur. Nostrud duis venison shankle beef ribs aliqua ut pariatur drumstick consectetur quis in. Swine anim ut ribeye jerky bresaola. Pork loin enim aute, corned beef pig prosciutto quis consequat. Tongue ham hock ball tip in ut laborum. Laboris et deserunt pork, corned beef non shoulder leberkas hamburger laborum ground round biltong.
+            </p>
+        </div>
     </div>
 </div>
