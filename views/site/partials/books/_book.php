dd74f5ada8e96599cf47ae324bfe20b2daa1926b
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-19 14:06:11 +0100

    Books - reponsive fixing

--- views/site/partials/books/_book.php
+++ views/site/partials/books/_book.php
@@ -1,30 +1,23 @@
 <?php
 use yii\helpers\Html;
 ?>
-<div class="col-md-12">
-    <div class="book media">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="media-left">
-                    <img src="<?= Yii::getAlias(Html::encode($book['image'])) ?>"
-                        alt="<?= Html::encode($book['title'])?>"
-                        class="img-thumbnail media-object pull-left" width=300 />
-                </div>
-
+<div class="book">
+    <div class="row">
+        <div class="col-md-4 col-sm-6 col-xs-12">
+            <img src="<?= Yii::getAlias(Html::encode($book['image'])) ?>"
+                alt="<?= Html::encode($book['title'])?>"
+                class="img-thumbnail img-responsive" width=300 />
+        </div>
+        <div class="col-md-8 col-sm-6 col-xs-12">
+            <h2 class="media-heading"><?= Html::encode($book['title'])?></h2>
+            <div class="author"> by <?= Html::encode($book['author'])?> </div>
+            <p>
+                <?= Html::encode($book['description'])?>
+            </p>
+            <div>
+                <a href="<?= Html::encode($book['url'])?>"><?= Html::encode($book['title'])?></a>
             </div>
-            <div class="col-md-8">
-                <div class="media-body">
-                    <h2 class="media-heading"><?= Html::encode($book['title'])?></h2>
-                    <div class="author"> by <?= Html::encode($book['author'])?> </div>
-                    <p>
-                        <?= Html::encode($book['description'])?>
-                    </p>
-                    <div>
-                        <a href="<?= Html::encode($book['url'])?>"><?= Html::encode($book['title'])?></a>
-                    </div>
-                    <div class="<?= Html::encode($book['level'])?>"><?= Html::encode($book['level-description'])?></div>
-                </div>
-            </div>
+            <div class="<?= Html::encode($book['level'])?>"><?= Html::encode($book['level-description'])?></div>
         </div>
     </div>
 </div> <!-- book -->
