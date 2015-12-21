31e7a1cf2453d281b64f8b09484be1ee8ee8ed67
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-22 00:35:15 +0100

    Front: features section

--- views/site/partials/index/_featurepoints.php
+++ views/site/partials/index/_featurepoints.php
@@ -1,14 +1,14 @@
-<div class="container content-separator wow fadeInUp">
+<div class="container content-separator wow fadeInUp features">
     <div class="row">
         <div class="col-md-4">
             <div class="media">
               <div class="media-left">
                 <a href="#">
-                  <img class="media-object" src="..." alt="...">
+                  <img class="media-object" src="/image/front/fast-80.png" alt="Yii is Fast">
                 </a>
               </div>
               <div class="media-body">
-                <div class="media-heading">Fast</div>
+                <div class="media-heading fast">Fast</div>
                 Yii gives you the maximum functionality by adding the least possible overhead
               </div>
             </div>
@@ -17,11 +17,11 @@
             <div class="media">
               <div class="media-left">
                 <a href="#">
-                  <img class="media-object" src="..." alt="...">
+                    <img class="media-object" src="/image/front/secure-80.png" alt="Yii is Secure">
                 </a>
               </div>
               <div class="media-body">
-                <div class="media-heading">Secure</div>
+                <div class="media-heading secure">Secure</div>
                     Sane defaults and built in tools helps you write solid and secure code
               </div>
             </div>
@@ -30,11 +30,11 @@
             <div class="media">
               <div class="media-left">
                 <a href="#">
-                  <img class="media-object" src="..." alt="...">
+                    <img class="media-object" src="/image/front/professional-80.png" alt="Yii is Efficient">
                 </a>
               </div>
               <div class="media-body">
-                <div class="media-heading">Efficient</div>
+                <div class="media-heading efficient">Efficient</div>
                     Write more code in less time with simple, yet powerful APIs and code generation
               </div>
             </div>
