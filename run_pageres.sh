1c697c97ef93ffee3cdf61fb0cfab259863d5d96
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-22 03:24:55 +0100

    Pageres: more appropriate sizes to capture

--- run_pageres.sh
+++ run_pageres.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd shots
-pageres http://local.yiiframework.com/ 240x768 280x768 320x768 600x768 768x768 1024x768 1280x768 1366x768
-#pageres http://local.yiiframework.com/doc/guide/2.0/en 240x768 280x768 320x768 600x768 768x768 1024x768 1280x768 1366x768
-#pageres http://local.yiiframework.com/doc/guide/2.0/en/start-installation 240x768 280x768 320x768 600x768 768x768 1024x768 1280x768 1366x768
-#pageres http://local.yiiframework.com/wiki 240x768 280x768 320x768 600x768 768x768 1024x768 1280x768 1366x768
+pageres http://local.yiiframework.com/ 320x480 360x640 768x1024 800x1280 980x1280 1280x600 1920x900
+pageres http://local.yiiframework.com/doc/guide/2.0/en 320x480 360x640 768x1024 800x1280 980x1280 1280x600 1920x900
+pageres http://local.yiiframework.com/doc/guide/2.0/en/start-installation 320x480 360x640 768x1024 800x1280 980x1280 1280x600 1920x900
+pageres http://local.yiiframework.com/wiki 320x480 360x640 768x1024 800x1280 980x1280 1280x600 1920x900
