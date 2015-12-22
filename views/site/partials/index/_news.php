d33a1cf81901f398c625d8a0cf8f9afbbcbb4f8f
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-22 02:29:21 +0100

    Front - news: link is styled as a button but the role is not

--- views/site/partials/index/_news.php
+++ views/site/partials/index/_news.php
@@ -16,7 +16,7 @@
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-            <a href="#" class="btn btn-block" role="button">Read all news</a>
+            <a href="#" class="btn btn-block">Read all news</a>
         </div>
     </div>
 </div>
