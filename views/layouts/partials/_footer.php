3e0f82cc12ff1a0c3871dca351f01941ee918b95
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-22 04:47:24 +0100

    Footer responsive-fied.

--- views/layouts/partials/_footer.php
+++ views/layouts/partials/_footer.php
@@ -2,19 +2,33 @@
     <footer>
         <div class="container">
             <div class="footer-inner">
-
-                <?= $this->render('footer/_about'); ?>
-
-                <?= $this->render('footer/_downloads'); ?>
-
-                <?= $this->render('footer/_documentation'); ?>
-
-                <?= $this->render('footer/_development'); ?>
-
-                <?= $this->render('footer/_community'); ?>
-
-                <?= $this->render('footer/_socialcopyright'); ?>
-
+                <div class="row">
+                    <div class="col-md-2 col-sm-4 hidden-xs">
+                        <?= $this->render('footer/_about'); ?>
+                    </div>
+                    <div class="col-md-2 col-sm-4 hidden-xs">
+                        <?= $this->render('footer/_downloads'); ?>
+                    </div>
+                    <div class="col-md-2 col-sm-4 hidden-xs">
+                        <?= $this->render('footer/_documentation'); ?>
+                    </div>
+                    <div class="col-md-2 col-sm-4 hidden-xs">
+                        <?= $this->render('footer/_development'); ?>
+                    </div>
+                    <div class="col-md-2 col-sm-4 hidden-xs">
+                        <?= $this->render('footer/_community'); ?>
+                    </div>
+                    <div class="col-md-2 col-sm-4 hidden-xs">
+                        <?= $this->render('footer/_socialcopyright'); ?>
+                    </div>
+                    <div class="col-xs-12 hidden-sm hidden-md hidden-lg">
+                        <div class="center-block">
+                            <div style="width:300px;">
+                                <?= $this->render('footer/_socialcopyright'); ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
