12bbd762e238420e80815d61e46bd57303064664
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-22 03:12:22 +0100

    Pageres script for testing

---
+++ run_pageres.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd shots
+pageres http://local.yiiframework.com/ 240x768 280x768 320x768 600x768 768x768 1024x768 1280x768 1366x768
+#pageres http://local.yiiframework.com/doc/guide/2.0/en 240x768 280x768 320x768 600x768 768x768 1024x768 1280x768 1366x768
+#pageres http://local.yiiframework.com/doc/guide/2.0/en/start-installation 240x768 280x768 320x768 600x768 768x768 1024x768 1280x768 1366x768
+#pageres http://local.yiiframework.com/wiki 240x768 280x768 320x768 600x768 768x768 1024x768 1280x768 1366x768
