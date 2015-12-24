c66bae55698b8df736b676335322755702a8866e
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-24 01:03:52 +0100

    Common heading fixed up

--- views/site/partials/common/_heading.php
+++ views/site/partials/common/_heading.php
@@ -2,7 +2,7 @@
 use yii\helpers\Html;
 ?>
 <div class="guide-header-wrap">
-    <div class="container guide-header">
+    <div class="container guide-header common-heading">
         <div class="row">
             <div class="col-md-12">
                 <h1 class="guide-headline"><?= Html::encode($title) ?></h1>
