05be9f0d6f8f56fbef27ca646a67f17d81110099
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-26 12:16:22 +0100

    Comments - more progress

--- components/views/comments.php
+++ components/views/comments.php
@@ -27,25 +27,25 @@ use yii\helpers\Html;
                         <div class="col-md-12">
                             <div class="comment">
                                 <div class="comment-header">
-                                    dlksajflkdsadjflksafjlfkadsjlfkj
+                                    <div class="row" id="c<?= $comment->id ?>">
+                                        <div class="col-md-1">
+                                            <a href="#c<?= $comment->id ?>" class="comment-id">#<?= $comment->id ?></a>
+                                        </div>
+                                        <div class="col-md-9 details">
+                                            <a href="#"><?= $comment->user->username; ?></a> at
+                                            <span class="date"><?=Yii::$app->formatter->format($comment->created_at, 'datetime')?></span>
+                                        </div>
+                                        <div class="col-md-1 voting votes-up">
+                                            <span class="votes">0</span> <i class="fa fa-thumbs-up"></i>
+                                        </div>
+                                        <div class="col-md-1 voting votes-down">
+                                            <span class="votes">0</span> <i class="fa fa-thumbs-down"></i>
+                                        </div>
+                                    </div>
                                 </div>
                                 <div class="comment-body">
-                                    <div class="author" id="c<?= $comment->id ?>">
-                                        <?php echo \cebe\gravatar\Gravatar::widget([
-                                            'email' => $comment->user->email,
-                                            'options' => [
-                                                'alt' => $comment->user->username,
-                                            ],
-                                            'size' => 32
-                                        ]); ?>
-                                        <?= Html::encode($comment->user->username) ?>
-                                    </div>
                                     <div class="text">
                                         <?= \yii\helpers\Markdown::process($comment->text) ?>
-                                    </div>
-                                    <div class="stuff">
-                                        <a href="#c<?= $comment->id ?>">#<?= $comment->id ?></a>
-                                        <span class="date"><?=Yii::$app->formatter->format($comment->created_at, 'datetime')?></span>
                                     </div>
                                 </div>
                             </div>
@@ -53,18 +53,26 @@ use yii\helpers\Html;
                     </div>
                 <?php endforeach ?>
             <?php endif ?>
-
-            <?php if (!Yii::$app->user->isGuest): ?>
-                <?php $form = ActiveForm::begin(); ?>
-                    <?= $form->field($commentForm, 'text')->label('Add new comment')->textarea() ?>
-
-                    <div class="form-group">
-                        <?= Html::submitButton('Comment', ['class' => 'btn btn-primary']) ?>
-                    </div>
-                <?php ActiveForm::end(); ?>
-            <?php else: ?>
-                <p><?= Html::a('Signup', ['site/signup'])?> or <?= Html::a('Login', ['site/login']) ?> in order to comment.</p>
-            <?php endif ?>
         </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-offset-2 col-md-9">
+        <span class="heading">Leave a comment</span>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-offset-2 col-md-9">
+        <?php if (Yii::$app->user->isGuest): ?>
+            <?php $form = ActiveForm::begin(); ?>
+                <?= $form->field($commentForm, 'text')->label('')->textarea() ?>
+
+                <div class="form-group">
+                    <?= Html::submitButton('Comment', ['class' => 'btn btn-primary']) ?>
+                </div>
+            <?php ActiveForm::end(); ?>
+        <?php else: ?>
+            <p><?= Html::a('Signup', ['site/signup'])?> or <?= Html::a('Login', ['site/login']) ?> in order to comment.</p>
+        <?php endif ?>
     </div>
 </div>
