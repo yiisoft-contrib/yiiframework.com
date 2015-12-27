f68b475b54464a7ac4354556ef51e055962eb936
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-27 12:14:48 +0100

    Guide: next, prev and up arrows - and appropriate styling

--- views/guide/view.php
+++ views/guide/view.php
@@ -94,14 +94,14 @@ $this->registerJs('
                 <div class="prev-next clearfix">
                     <?php
                     if (($prev = $section->getPrevSection()) !== null) {
-                        $left = '<span class="fa fa-chevron-left" aria-hidden="true"></span> ';
+                        $left = '<span class="chevron-left" aria-hidden="true"></span> ';
                         echo '<div class="pull-left">' . Html::a($left . Html::encode($prev[1]), ['guide/view', 'section' => $prev[0], 'version' => $guide->version, 'language' => $guide->language, 'type' => $guide->typeUrlName]) . '</div>';
                     }
                     if (($next = $section->getNextSection()) !== null) {
-                        $right = ' <span class="fa fa-chevron-right" aria-hidden="true"></span>';
+                        $right = ' <span class="chevron-right" aria-hidden="true"></span>';
                         echo '<div class="pull-right">' . Html::a(Html::encode($next[1]) . $right, ['guide/view', 'section' => $next[0], 'version' => $guide->version, 'language' => $guide->language, 'type' => $guide->typeUrlName]) . '</div>';
                     }
-                    echo '<div class="text-center"><a href="#top">Go to Top <span class="fa fa-chevron-up" aria-hidden="true"></span></a></div>';
+                    echo '<div class="text-center"><a href="#top">Go to Top <span class="chevron-up" aria-hidden="true"></span></a></div>';
                     ?>
                 </div>
 
