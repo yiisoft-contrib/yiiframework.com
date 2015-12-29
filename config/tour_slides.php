7cb4b1c44583220be57f4ff291c500db0251eb72
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-29 12:58:46 +0100

    Tour removed

--- config/tour_slides.php
+++ config/tour_slides.php
@@ -1,49 +1,13 @@
 <?php
 $content_1 = <<<EOD1
-Composer content here
+content here
 EOD1;
-$content_2 = <<<EOD2
-Template content here
-EOD2;
-$content_3 = <<<EOD3
-Welcome content here
-EOD3;
-$content_4 = <<<EOD4
-Migrations content here
-EOD4;
-$content_5 = <<<EOD5
-Gii content here
-EOD5;
 
 return [
     [
-        'title' => 'Composer',
-        'sub_title' => 'Step 1',
-        'image' => '@web/image/tour/composer.png',
+        'title' => 'Ipsum',
+        'sub_title' => 'Lorem',
+        'image' => '@web/image/tour-gii.png',
         'content' => $content_1,
-    ],
-    [
-        'title' => 'Basic project template',
-        'sub_title' => 'Step 2',
-        'image' => '@web/image/tour/project-install.png',
-        'content' => $content_2,
-    ],
-    [
-        'title' => 'Welcome page',
-        'sub_title' => 'Step 3',
-        'image' => '@web/image/tour/start-app-installed.png',
-        'content' => $content_3,
-    ],
-    [
-        'title' => 'Migrations',
-        'sub_title' => 'Step 4',
-        'image' => '@web/image/tour/migration.png',
-        'content' => $content_4,
-    ],
-    [
-        'title' => 'Gii - Yii Code Generator',
-        'sub_title' => 'Step 5',
-        'image' => '@web/image/tour/gii.png',
-        'content' => $content_5,
     ],
 ];
