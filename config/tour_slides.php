5b37da276f9bb29fc8b23aedee7ec9959d8f780d
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-29 13:51:04 +0100

    Front -  tour: slides upped to 5 to maintain layout

--- config/tour_slides.php
+++ config/tour_slides.php
@@ -6,7 +6,31 @@ EOD1;
 return [
     [
         'title' => 'Ipsum',
-        'sub_title' => 'Lorem',
+        'sub_title' => 'Lorem 1',
+        'image' => '@web/image/front/tour-gii.png',
+        'content' => $content_1,
+    ],
+    [
+        'title' => 'Ipsum',
+        'sub_title' => 'Lorem 2',
+        'image' => '@web/image/front/tour-gii.png',
+        'content' => $content_1,
+    ],
+    [
+        'title' => 'Ipsum',
+        'sub_title' => 'Lorem 3',
+        'image' => '@web/image/front/tour-gii.png',
+        'content' => $content_1,
+    ],
+    [
+        'title' => 'Ipsum',
+        'sub_title' => 'Lorem 4',
+        'image' => '@web/image/front/tour-gii.png',
+        'content' => $content_1,
+    ],
+    [
+        'title' => 'Ipsum',
+        'sub_title' => 'Lorem 5',
         'image' => '@web/image/front/tour-gii.png',
         'content' => $content_1,
     ],
