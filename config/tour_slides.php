b261fb2b1a4a28d0d25e133f6efdd66073fc5229
Jacob Moen <jacmoe.dk@gmail.com>
2015-12-30 09:01:50 +0100

    Front - tour: while we wait for real images - placeholder

--- config/tour_slides.php
+++ config/tour_slides.php
@@ -5,33 +5,33 @@ EOD1;
 
 return [
     [
-        'title' => 'Ipsum',
-        'sub_title' => 'Lorem 1',
+        'title' => 'Installation',
+        'sub_title' => 'Step 1',
+        'image' => '@web/image/front/slide.png',
+        'content' => $content_1,
+    ],
+    [
+        'title' => 'Welcome Page',
+        'sub_title' => 'Step 2',
+        'image' => '@web/image/front/slide.png',
+        'content' => $content_1,
+    ],
+    [
+        'title' => 'Migrations',
+        'sub_title' => 'Step 3',
+        'image' => '@web/image/front/slide.png',
+        'content' => $content_1,
+    ],
+    [
+        'title' => 'Gii code generator',
+        'sub_title' => 'Step 4',
         'image' => '@web/image/front/tour-gii.png',
         'content' => $content_1,
     ],
     [
-        'title' => 'Ipsum',
-        'sub_title' => 'Lorem 2',
-        'image' => '@web/image/front/tour-gii.png',
-        'content' => $content_1,
-    ],
-    [
-        'title' => 'Ipsum',
-        'sub_title' => 'Lorem 3',
-        'image' => '@web/image/front/tour-gii.png',
-        'content' => $content_1,
-    ],
-    [
-        'title' => 'Ipsum',
-        'sub_title' => 'Lorem 4',
-        'image' => '@web/image/front/tour-gii.png',
-        'content' => $content_1,
-    ],
-    [
-        'title' => 'Ipsum',
-        'sub_title' => 'Lorem 5',
-        'image' => '@web/image/front/tour-gii.png',
+        'title' => 'Where to go next',
+        'sub_title' => 'Step 5',
+        'image' => '@web/image/front/slide.png',
         'content' => $content_1,
     ],
 ];
