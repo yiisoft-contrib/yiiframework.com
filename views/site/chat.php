f8fae7961c7211f4cc6d5bef3f31ec63bf2722c4
Carsten Brandt <mail@cebe.cc>
2016-05-12 00:23:10 +0200

    experiment with KIWI IRC chat implementation

--- views/site/chat.php
+++ views/site/chat.php
@@ -1,5 +1,8 @@
 <?php
 use yii\helpers\Html;
+use yii\web\View;
+
+Yii::setAlias('@kiwi', __DIR__ . '/../../../KiwiIRC');
 
 /* @var $this yii\web\View */
 $this->title = 'Live Chat';
@@ -25,7 +28,33 @@ $this->params['breadcrumbs'][] = $this->title;
                 <a href="http://www.yiiframework.com/wiki/369/" target="_blank">Instructions on using MrFisk</a>
             </p>
 
+            <?php /*
             <iframe src="http://webchat.freenode.net?channels=yii&uio=OT10cnVlJjExPTE5NSYxMj10cnVl09" width="100%" height="480" style="border:1px solid silver;"></iframe>
+
+            <iframe src="https://kiwiirc.com/client/irc.freenode.net/?theme=mini#yii" style="border:0; width:100%; height:480px;"></iframe>
+            */ ?>
+
+
+            <?php
+
+            if (preg_match('~<body>(.*)</body>~si', file_get_contents(Yii::getAlias('@kiwi/client/index.html')), $matches)) {
+                $this->registerMetaTag(['name' => 'referrer', 'content' => 'origin-when-crossorigin']);
+
+                $kiwiHtml = $matches[1];
+                // remove jquery, it is already here
+                $kiwiHtml = preg_replace('~<script src=".+?jquery.+?js"></script>~', '', $kiwiHtml);
+                // set base path for the kiwi server
+                $kiwiHtml = preg_replace('~var base_path = \'/kiwi\'~', 'var base_path = \'http://localhost:7778/kiwi\'', $kiwiHtml);
+
+                Yii::$app->assetManager->bundles[app\assets\AppAsset::class]['jsOptions']['position'] = View::POS_BEGIN;
+
+                echo $kiwiHtml;
+
+            } else {
+                throw new \yii\base\Exception('Unable to extract Kiwi template data.');
+            }
+
+            ?>
         </div>
     </div>
 </div>
