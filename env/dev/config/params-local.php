1cbccf894a18100fbc608e0dd677cc5fb6516b0e
Carsten Brandt <mail@cebe.cc>
2016-09-12 11:44:06 +0200

    added more settings to params.local.php

--- env/dev/config/params-local.php
+++ env/dev/config/params-local.php
@@ -1,11 +1,29 @@
 <?php
 return [
+    // if you develop yiiframework.com in a subdirectory under localhost
+    // and not on a separate host name, enter the baseUrl here
+//    'api.baseUrl' => '/doc/api',
+//    'guide.baseUrl' => '/doc/guide',
+//    'blogtut.baseUrl' => '/doc/blog',
+
+    'components.db' => [
+        'class' => 'yii\db\Connection',
+        'charset' => 'utf8',
+
+        // adjust DB credentials to your needs
+        'dsn' => 'mysql:host=localhost;dbname=yiiframeworkcom',
+        'username' => 'yiiframeworkcom',
+        'password' => 'yiiframeworkcom',
+    ],
+
     'authclients' => [
         'github' => [
             'class' => 'yii\authclient\clients\GitHub',
+            'scope' => 'user:email',
+
+            // register a new application on Github and enter client secrets here
             'clientId' => '',
             'clientSecret' => '',
-            'scope' => 'user:email',
         ],
     ],
 ];
