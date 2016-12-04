a974285b8be30cf18768495ae3663d71adbf45fe
Carsten Brandt <mail@cebe.cc>
2016-12-04 21:39:23 +0100

    cleanup

--- views/wiki/index.php
+++ views/wiki/index.php
@@ -37,83 +37,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 'itemView' => '_view',
             ]) ?>
 
-<!-- delete from here -- >
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-            <div class="row">
-                <div class="col-md-12 col-lg-9">
-                    <div class="content wiki-row">
-                        <div class="suptitle">Created 17 days ago by <a href="#">AlaFalaki</a></div>
-                        <h2 class="title"><a href="#">How to change GridView delete confirmation message specific controllers only?</a></h2>
-                        <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-                        <div class="comments"><a href="#">0 comments</a></div>
-                    </div>
-                </div> 
-                <div class="col-md-12 col-lg-3">
-                    <div class="vote-box content">
-                        <div class="thumbs">
-                        <span class="up">
-                            <span class="votes">1</span> 
-                            <a href="#"><i class="fa fa-thumbs-up" aria-hidden="true"></i></a>
-                        </span>
-                        <span class="down">
-                            <span class="votes">0</span>
-                            <a href="#"><i class="fa fa-thumbs-down" aria-hidden="true"></i></a>
-                        </span>
-                        </div>
-                        <div class="viewed"><span>Viewed:</span> 1 452 times</div>
-                        <div class="version"><span>Version:</span> 2.0</div>
-                        <div class="group"><span>Group:</span> <a href="#">Tips</a></div>
-                        <div class="tags"><span>Tags:</span> <a href="#">gridview</a>, <a href="#">delete</a>, <a href="#">message</a></div>
-                    </div>
-                </div>              
-            </div>
-            <div class="row">
-                <div class="col-md-12 col-lg-9">
-                    <div class="content wiki-row">
-                        <div class="suptitle">Created 17 days ago by <a href="#">AlaFalaki</a></div>
-                        <h2 class="title"><a href="#">How to change GridView delete confirmation message specific controllers only?</a></h2>
-                        <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-                        <div class="comments"><a href="#">0 comments</a></div>
-                    </div>
-                </div> 
-                <div class="col-md-12 col-lg-3">
-                    <div class="vote-box content">
-                        <div class="thumbs">
-                        <span class="up">
-                            <span class="votes">1</span> 
-                            <a href="#"><i class="fa fa-thumbs-up" aria-hidden="true"></i></a>
-                        </span>
-                        <span class="down">
-                            <span class="votes">0</span>
-                            <a href="#"><i class="fa fa-thumbs-down" aria-hidden="true"></i></a>
-                        </span>
-                        </div>
-                        <div class="viewed"><span>Viewed:</span> 1 452 times</div>
-                        <div class="version"><span>Version:</span> 2.0</div>
-                        <div class="group"><span>Group:</span> <a href="#">Tips</a></div>
-                        <div class="tags"><span>Tags:</span> <a href="#">gridview</a>, <a href="#">delete</a>, <a href="#">message</a></div>
-                    </div>
-                </div>              
-            </div>
-<!-- delete to here -->
-
-            <nav class="wiki-pagination-holder">
+<!--            <nav class="wiki-pagination-holder">
               <ul class="pagination pagination-lg wiki-pagination">
                 <li class="prev disabled">
                     <span><i class="fa fa-chevron-left" aria-hidden="true"></i></span>
@@ -132,7 +56,7 @@ proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     <a href="#"><i class="fa fa-chevron-right" aria-hidden="true"></i></a>
                 </li>
               </ul>
-            </nav>
+            </nav>-->
 
         </div>
     </div>
