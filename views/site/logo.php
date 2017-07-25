e3f852a7a0075e6c2b48cb18217037706e15147b
Alexander Makarov <sam@rmcreative.ru>
2017-07-26 02:30:32 +0300

    Used colored logos for logo, assets etc. + cleanup of unused images

--- views/site/logo.php
+++ views/site/logo.php
@@ -20,19 +20,19 @@ $this->params['breadcrumbs'][] = $this->title;
            <div>
                <h2>Logo for dark backgrounds</h2>
                <div class="logo-download-dark">
-                   <img src="<?= Yii::getAlias('@web/image/yii_logo.svg') ?>" height="80" title="Yii logo for dark backgrounds" alt="Yii logo for dark backgrounds">
-                    transparent background, 725 x 157 px.
+                   <img src="<?= Yii::getAlias('@web/image/yii_logo_dark.svg') ?>" height="80" title="Yii logo for dark backgrounds" alt="Yii logo for dark backgrounds">
+                    transparent background, 725 x 149 px.
                </div>
 
-               <p>Download: <a href="<?= Yii::getAlias('@web/image/yii_logo.svg') ?>">SVG version</a>, <a href="<?= Yii::getAlias('@web/files/logo/yii_logo.png') ?>">PNG version</a></p>
+               <p>Download: <a href="<?= Yii::getAlias('@web/image/yii_logo_dark.svg') ?>">SVG version</a>, <a href="<?= Yii::getAlias('@web/image/yii_logo_dark.png') ?>">PNG version</a></p>
            </div>
            <div>
                <h2>Logo for light backgrounds</h2>
                <div class="logo-download-light">
-                   <img src="<?= Yii::getAlias('@web/image/yii_logo_blue.svg') ?>" height="80" title="Yii logo for light backgrounds" alt="Yii logo for light backgrounds">
-                    transparent background, 725 x 157 px.
+                   <img src="<?= Yii::getAlias('@web/image/yii_logo_light.svg') ?>" height="80" title="Yii logo for light backgrounds" alt="Yii logo for light backgrounds">
+                    transparent background, 725 x 149 px.
                </div>
-               <p>Download: <a href="<?= Yii::getAlias('@web/image/yii_logo_blue.svg') ?>">SVG version</a>, <a href="<?= Yii::getAlias('@web/files/logo/yii_logo_blue.png') ?>">PNG version</a></p>
+               <p>Download: <a href="<?= Yii::getAlias('@web/image/yii_logo_light.svg') ?>">SVG version</a>, <a href="<?= Yii::getAlias('@web/image/yii_logo_light.png') ?>">PNG version</a></p>
            </div>
        </div>
     </div>
