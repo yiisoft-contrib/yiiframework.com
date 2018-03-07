0b820fe8b23f9d9cd755c2cc4dda05b97d0b8c54
Carsten Brandt <mail@cebe.cc>
2018-03-07 03:08:38 +0100

    add redis session and cache in production

--- env/prod/config/web-local.php
+++ env/prod/config/web-local.php
@@ -8,6 +8,20 @@ return [
         'request' => [
             'cookieValidationKey' => '',
         ],
+        'session' => [
+            'class' => yii\redis\Session::class,
+            'redis' => [
+                'class' => yii\redis\Connection::class,
+                'database' => 0,
+            ],
+        ],
+        'cache' => [
+            'class' => yii\redis\Cache::class,
+            'redis' => [
+                'class' => yii\redis\Connection::class,
+                'database' => 1,
+            ],
+        ],
         'errorHandler' => [
             'class' => 'baibaratsky\yii\rollbar\web\ErrorHandler',
             'payloadDataCallback' => function (\baibaratsky\yii\rollbar\web\ErrorHandler $errorHandler) {
