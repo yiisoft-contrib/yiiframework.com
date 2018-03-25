38b36a212ac2e4d3c31265999dc7bad614803b71
Alexander Makarov <sam@rmcreative.ru>
2018-03-25 21:09:16 +0300

    Download page adjustments:
    - Less text
    - No fxp
    - Mentioned composer for 1.1
    - Linked yiisoft/yii to packagist
    - Added HTML hint to doc archive titles
    
    close #291

--- views/site/download.php
+++ views/site/download.php
@@ -26,44 +26,44 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="container download">
 	<div class="content">
 		<div class="row">
+            <div class="col-md-12">
+                <p class="small">
+                    Yii is an open source project released under the terms of the <?= Html::a('BSD License', ['site/license']) ?>.
+				    This means that you can use Yii for free to develop either open-source or proprietary Web applications.
+                </p>
+                
+                <p class="small">
+                    There are two ways to install Yii:
+                    by using Composer or
+                    by downloading an application template.
+                    We highly recommend you to use Composer.
+                </p>
+
+                <p class="small">
+                    If you do not have Composer installed yet,
+                    you may install it following the instructions
+                    on the <a href="https://getcomposer.org/download/">Composer website</a>.
+                </p>
+            </div>
+
 			<div class="col-md-12">
-				<p>
-					Yii is an open source project released under the terms of the <?= Html::a('BSD License', ['site/license']) ?>.
-					This means that you can use Yii for free to develop either open-source or proprietary Web applications.
-				</p>
-				<p id="yii-2-0">
-					On this page you find download options for the current two major versions of Yii,
-					<a href="#yii-2-0">version 2.0</a> and <a href="#yii-1-1">version 1.1</a> as well as an <a href="#archive">archive</a> of old resources.
-				</p>
 				<div class="heading-separator">
 					<h2><span>Yii 2.0</span></h2>
 				</div>
 				<?php $version = '2.0'; ?>
 				<p class="small">
-				The latest release of Yii 2 is <strong><?= $latest = key($versions[$version]) ?></strong>
-				released on <strong><?= $versions[$version][$latest] ?></strong>.
-				For changes in this and older versions, see the <?= Html::a('CHANGELOG', "https://github.com/yiisoft/yii2/blob/$latest/framework/CHANGELOG.md") ?> file.
-				Instructions on how to upgrade to this version can be found in the <?= Html::a('UPGRADE', "https://github.com/yiisoft/yii2/blob/$latest/framework/UPGRADE.md") ?> file.
 
-				A list of all Yii 2.x releases can be found on the <a href="https://github.com/yiisoft/yii2/releases">github repository</a>.
-				</p>
+                <ul>
+                    <li>Latest release is <strong><?= $latest = key($versions[$version]) ?></strong> (<strong><?= $versions[$version][$latest] ?></strong>)</li>
+                    <li><?= Html::a('Changes in this and older versions', "https://github.com/yiisoft/yii2/blob/$latest/framework/CHANGELOG.md") ?></li>
+                    <li><?= Html::a('Upgrade instructions', "https://github.com/yiisoft/yii2/blob/$latest/framework/UPGRADE.md") ?></li>
+                    <li><a href="https://github.com/yiisoft/yii2/releases">List of all Yii 2.x releases</a></li>
+                </ul>
 
-<!--            <div class="row text-center ptrem2">
-					<div class="col-md-12">
-					<p class="small"><a href="#changelog">Complete change log</a><span class="separator">|</span><a href="#upgrade">Upgrade instructions</a></p>
-					</div>
-				</div>-->
+
+				<h1 id="install-composer">Install via Composer (recommended)</h1>
 
 				<p class="small">
-					There are two ways to install Yii 2:
-					by <a href="#install-composer">using Composer</a> or
-					by <a href="#install-from-archive">downloading an application template</a>.
-					We highly recommend you to use Composer.
-				</p>
-
-				<h1 id="install-composer">Install via Composer</h1>
-
-				<p class="small">This is the recommended way of installing Yii 2.0.
 				The installation instructions described here are a short summary, you may want to check the
 					<?= Html::a('Definitive Guide', [
 						'guide/view',
@@ -74,22 +74,16 @@ $this->params['breadcrumbs'][] = $this->title;
 						'#' => 'installing-via-composer',
 					]) ?> for more detailed instructions.</p>
 
-				<p class="small">If you do not have Composer installed yet, you may install it following the instructions on the <a href="https://getcomposer.org/download/">Composer website</a>.</p>
-
-				<p class="small">After installing Composer, run the following command to install the <a target="_blank" rel="noopener noreferrer" href="https://github.com/francoispluchino/composer-asset-plugin">Composer Asset Plugin</a>, which is required by Yii 2.0:</p>
-
-				<pre><code class="hljs bash language-bash">php composer.phar global require "fxp/composer-asset-plugin:^1.4.2"</code></pre>
-
 				<p class="small">
-					Now choose one of the application templates to start installing Yii 2.0.
+					Choose one of the application templates to start installing Yii 2.0.
 					An application template is a package that contains a skeleton Web application written in Yii
-					which you can start from building your application on it.
+					which you can start building your application from.
 					If you just start with Yii, we recommend the installation of the <strong>basic</strong> template.
 				</p>
 
 				<div class="row ptrem1">
 					<div class="col-md-3">
-					<p class="small install">To install the <b>basic</b> application template, run the following command:</p>
+					<p class="small">To install the <b>basic</b> application template, run the following command:</p>
 					</div>
 					<div class="col-md-9">
 					<pre><code class="hljs bash language-bash">php composer.phar create-project yiisoft/yii2-app-basic basic</code></pre>
@@ -110,7 +104,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
 				<div class="row ptrem1">
 					<div class="col-md-3">
-					<p class="small install">To install the <b>advanced</b> application template, run the following command:</p>
+					<p class="small">To install the <b>advanced</b> application template, run the following command:</p>
 					</div>
 					<div class="col-md-9">
 					<pre><code class="hljs bash language-bash">php composer.phar create-project yiisoft/yii2-app-advanced advanced</code></pre>
@@ -118,7 +112,7 @@ $this->params['breadcrumbs'][] = $this->title;
 				</div>
 
 				<p class="small">
-					When you have installed the advanced application template, you should check out its <a href="https://github.com/yiisoft/yii2-app-advanced/tree/master/docs/guide#readme">documentation</a>
+					When you have installed the advanced application template, you should check out its <a href="<?= Yii::getAlias('@web/extension/yiisoft/yii2-app-advanced/doc/guide') ?>">documentation</a>
 					to learn how to use it.
 				</p>
 
@@ -149,19 +143,13 @@ $this->params['breadcrumbs'][] = $this->title;
 						'section' => 'start-installation',
 						'#' => 'verifying-installation',
 					]) ?> to get started, or
-					check out the <a href="https://github.com/yiisoft/yii2-app-advanced/tree/master/docs/guide#readme">documentation of the advanced application template</a>
+					check out the <a href="<?= Yii::getAlias('@web/extension/yiisoft/yii2-app-advanced/doc/guide') ?>">documentation of the advanced application template</a>
 					to learn how to use it.
 				</p>
 
 				<h1>Upgrade from Older Versions</h1>
 
 				<p class="small">If you are upgrading from Yii 2.0.x with Composer, simply run the following commands in your project's root directory:</p>
-
-				<p class="small">First, make sure you have the latest version of the Composer Asset Plugin:</p>
-
-				<pre><code class="hljs bash language-bash">php composer.phar global require "fxp/composer-asset-plugin:^1.4.2"</code></pre>
-
-				<p class="small">Then upgrade Yii and its dependencies by running:</p>
 
 				<pre><code class="hljs bash language-bash">php composer.phar update yiisoft/yii2 yiisoft/yii2-composer bower-asset/jquery.inputmask</code></pre>
 
@@ -241,30 +229,19 @@ $this->params['breadcrumbs'][] = $this->title;
 				</div>
 				<?php $version = '1.1'; ?>
 
-				<p class="text-center medium">Yii 1.1 is currently in <a href="/news/90/update-on-yii-1-1-support-and-end-of-life/">maintenance mode</a>.</p>
-				<p class="text-center">Continued support and bug fixes for this version have been provided until <b><?= $versionInfo[$version]['support-until'] ?></b>.</p>
-				<p class="text-center">Security fixes and PHP 7 compatibility until <b><?= $versionInfo[$version]['security-until'] ?></b>.</p>
+                <p class="text-center medium">Yii 1.1 is currently in <a href="/news/90/update-on-yii-1-1-support-and-end-of-life/">maintenance mode</a>.</p>
+                <p class="text-center">Continued support and bug fixes for this version have been provided until <b><?= $versionInfo[$version]['support-until'] ?></b>.</p>
+                <p class="text-center">Security fixes and PHP 7 compatibility until <b><?= $versionInfo[$version]['security-until'] ?></b>.</p>
 
-				<p class="small">
-					The latest release of Yii 1.1 is <strong><?= $latest = key($versions[$version]) ?></strong>
-					released on <strong><?= $versions[$version][$latest] ?></strong>.
-					For changes in this and older versions, see the <?= Html::a('CHANGELOG', "https://github.com/yiisoft/yii/blob/$latest/CHANGELOG") ?> file.
-					Instructions on how to upgrade to this version can be found in the <?= Html::a('UPGRADE', "https://github.com/yiisoft/yii/blob/$latest/UPGRADE") ?> file.
-				</p>
-
-				<div class="row text-center ptrem1">
-					<div class="col-md-12">
-						<p class="small">
-							<a href="<?= $versionInfo[$version]['download-url'] ?>.tar.gz">Source Code</a> (<a href="<?= $versionInfo['1.1']['download-url'] ?>.zip">.zip</a>)
-							<span class="separator"></span>
-							<a href="https://raw.githubusercontent.com/yiisoft/yii/<?= $latest ?>/CHANGELOG">Change log</a>
-							<span class="separator"></span>
-							<a href="https://raw.githubusercontent.com/yiisoft/yii/<?= $latest ?>/UPGRADE">Upgrade instructions</a>
-							<span class="separator"></span>
-							<a href="<?= str_replace('yii-', 'yii-docs-', $versionInfo[$version]['download-url']) ?>.tar.gz">Offline Documentation</a> (<a href="<?= str_replace('yii-', 'yii-docs-', $versionInfo[$version]['download-url']) ?>.zip">.zip</a>)
-						</p>
-					</div>
-				</div>
+                <ul>
+                    <li>Latest release is <strong><?= $latest = key($versions[$version]) ?></strong> (<strong><?= $versions[$version][$latest] ?></strong>)</li>
+                    <li><a href="<?= $versionInfo[$version]['download-url'] ?>.tar.gz">Source Code</a> (<a href="<?= $versionInfo['1.1']['download-url'] ?>.zip">.zip</a>)</li>
+                    <li><?= Html::a('Changes in this and older versions', "https://github.com/yiisoft/yii/blob/$latest/CHANGELOG") ?></li>
+                    <li><?= Html::a('Upgrade instructions', "https://github.com/yiisoft/yii/blob/$latest/UPGRADE") ?></li>
+                    <li><a href="<?= str_replace('yii-', 'yii-docs-', $versionInfo[$version]['download-url']) ?>.tar.gz">Offline Documentation</a>
+                        (<a href="<?= str_replace('yii-', 'yii-docs-', $versionInfo[$version]['download-url']) ?>.zip">.zip</a>)</li>
+                    <li><a href="https://github.com/yiisoft/yii/releases">List of all Yii 1.x releases</a></li>
+                </ul>
 
 				<p class="small">
 					You may obtain the latest development version of the Yii 1.1 code from the <?= Html::a('Github repository', $versionInfo[$version]['github-url']) ?> using git:
@@ -273,7 +250,7 @@ $this->params['breadcrumbs'][] = $this->title;
 				<p class="small">or via SVN using:</p>
 				<p><code>svn checkout <?= $versionInfo[$version]['svn-url'] ?> yii</code></p>
 
-				<p class="small">A list of all Yii 1.x releases can be found on github: <a href="https://github.com/yiisoft/yii/releases">https://github.com/yiisoft/yii/releases</a>.</p>
+                <p>The <a href="https://packagist.org/packages/yiisoft/yii">yiisoft/yii</a> composer package is available as well.</p>
 
 				<h3 class="text-center">Documentation</h3>
 
