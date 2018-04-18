d7fccc95d3f1180ca682d60bfc9e9d4f81523540
Carsten Brandt <mail@cebe.cc>
2018-04-18 09:38:41 +0200

    fix extension guide reference links

--- views/guide/extension-view.php
+++ views/guide/extension-view.php
@@ -85,11 +85,11 @@ echo $this->render('partials/_scrollspy.php', ['guide' => $guide, 'section' => $
                     <?php
                     if (($prev = $section->getPrevSection()) !== null) {
                         $left = '<span class="chevron-left" aria-hidden="true"></span> ';
-                        echo '<div class="pull-left">' . Html::a($left . Html::encode($prev[1]), ['guide/view', 'section' => $prev[0], 'version' => $guide->version, 'language' => $guide->language, 'type' => $guide->typeUrlName]) . '</div>';
+                        echo '<div class="pull-left">' . Html::a($left . Html::encode($prev[1]), ['guide/extension-view', 'section' => $prev[0], 'version' => $guide->version, 'language' => $guide->language, 'name' => $extensionName, 'vendorName' => $extensionVendor]) . '</div>';
                     }
                     if (($next = $section->getNextSection()) !== null) {
                         $right = ' <span class="chevron-right" aria-hidden="true"></span>';
-                        echo '<div class="pull-right">' . Html::a(Html::encode($next[1]) . $right, ['guide/view', 'section' => $next[0], 'version' => $guide->version, 'language' => $guide->language, 'type' => $guide->typeUrlName]) . '</div>';
+                        echo '<div class="pull-right">' . Html::a(Html::encode($next[1]) . $right, ['guide/extension-view', 'section' => $next[0], 'version' => $guide->version, 'language' => $guide->language, 'name' => $extensionName, 'vendorName' => $extensionVendor]) . '</div>';
                     }
                     echo '<div class="text-center"><a href="#top">Go to Top <span class="chevron-up" aria-hidden="true"></span></a></div>';
                     ?>
