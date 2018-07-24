a70e0e6361af302af46ff050b1e0d038f2a27ac2
Carsten Brandt <mail@cebe.cc>
2018-07-24 14:08:35 +0200

    oauth: do not disable client when logging in again

--- components/AuthHandler.php
+++ components/AuthHandler.php
@@ -66,7 +66,7 @@ class AuthHandler
                 $this->addAuthProvider($id, $login);
             } else { // there's existing auth
                 if ($auth->user_id == Yii::$app->user->id) {
-                    $this->disableClient($auth);
+                    // do nothing, login is already active
                 } else {
                     $this->clientAlreadyInUse();
                 }
@@ -256,27 +256,6 @@ class AuthHandler
                     'errors' => json_encode($auth->getErrors()),
                 ]),
             ]);
-        }
-    }
-
-    /**
-     * @param Auth $auth
-     * @throws \yii\db\Exception
-     */
-    private function disableClient(Auth $auth)
-    {
-        $transaction = Auth::getDb()->beginTransaction();
-        if ($auth->delete()) {
-            $user = $auth->user;
-            $user->{$this->client->getId()} = null;
-            if ($user->save()) {
-                $transaction->commit();
-                Yii::$app->getSession()->setFlash('success', [
-                    Yii::t('app',
-                        'Social network {client} has been successfully disabled.',
-                        ['client' => $this->client->getTitle()]),
-                ]);
-            }
         }
     }
 
