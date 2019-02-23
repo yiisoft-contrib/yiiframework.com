0724266698a6dbc66188058c18f326e9f0a9bbfa
Mehdi Achour <machour@gmail.com>
2019-02-23 15:36:01 +0100

    Fix FQNs

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -26,10 +26,10 @@ class SiteController extends BaseController
     {
         return [
             'error' => [
-                'class' => yii\web\ErrorAction::class,
+                'class' => \yii\web\ErrorAction::class,
             ],
             'captcha' => [
-                'class' => yii\captcha\CaptchaAction::class,
+                'class' => \yii\captcha\CaptchaAction::class,
                 'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
             ],
         ];
