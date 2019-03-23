4678d3d7f1cb05e10755922ee8b7b14e79bd95ec
Mehdi Achour <machour@gmail.com>
2019-03-23 13:22:06 +0100

    Wiki is now 100% accessible

--- views/wiki/_view.php
+++ views/wiki/_view.php
@@ -11,18 +11,18 @@ use yii\helpers\Html;
 <div class="row">
     <div class="col-md-12 col-lg-9">
         <div class="content wiki-row">
-            <div class="suptitle">
+            <h2 class="title"><?= Html::a(Html::encode($model->title), ['wiki/view', 'id' => $model->id, 'name' => $model->slug]) ?></h2>
+            <div class="subtitle">
                 Created <?= Yii::$app->formatter->asRelativeTime($model->created_at) ?> by <?= $model->creator->rankLink ?><?php
                 if ($model->updated_at !== null): ?>,
                     updated <?= Yii::$app->formatter->asRelativeTime($model->updated_at) ?> by <?= $model->updater->rankLink ?><?php
                 endif; ?>.
+                <div class="comments"><?= Html::a(
+                        Yii::$app->i18n->format('{n, number} {n, plural, one{comment} other{comments}}', ['n' => $model->comment_count], Yii::$app->language),
+                        ['wiki/view', 'id' => $model->id, 'name' => $model->slug, '#' => 'comments']
+                    ) ?></div>
             </div>
-            <h2 class="title"><?= Html::a(Html::encode($model->title), ['wiki/view', 'id' => $model->id, 'name' => $model->slug]) ?></h2>
             <div class="text"><?= $model->teaser ?></div>
-            <div class="comments"><?= Html::a(
-                    Yii::$app->i18n->format('{n, number} {n, plural, one{comment} other{comments}}', ['n' => $model->comment_count], Yii::$app->language),
-                    ['wiki/view', 'id' => $model->id, 'name' => $model->slug, '#' => 'comments']
-            ) ?></div>
         </div>
     </div>
     <div class="col-md-12 col-lg-3">
