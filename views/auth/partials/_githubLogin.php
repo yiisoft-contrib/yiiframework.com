112e2fb03342844ba4de63a91eb10033b8803bae
Ihor Sychevskyi <26163841+Arhell@users.noreply.github.com>
2020-03-18 15:00:26 +0200

    Update github social icon on login page (#489)

--- views/auth/partials/_githubLogin.php
+++ views/auth/partials/_githubLogin.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 ?>
 <div class="col-md-3 social-login">
       <span class="github-icon">
-          <i class="fa fa fa-github-square"></i>
+          <i class="fa fa-github"></i>
       </span>
       <h4>Did you sign up with your<br/>Github Account?</h4>
       <?= Html::a('Login with Github', ['auth/auth', 'authclient' => 'github'], ['class' => 'btn btn-lg']) ?>
