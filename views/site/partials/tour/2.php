52cd58d05b0844368972f10d561c89fc9f2e730c
Ihor Sychevskyi <26163841+Arhell@users.noreply.github.com>
2020-06-06 11:56:14 +0300

    update srcset (#544)

--- views/site/partials/tour/2.php
+++ views/site/partials/tour/2.php
@@ -3,7 +3,7 @@
         <div class="col-md-7">
             <img src="<?= Yii::getAlias('@web/image/tour/2.png') ?>"
                  alt="Welcome Page"
-                 srcset="<?= Yii::getAlias('@web/image/tour/2@2x.png') ?>"
+                 srcset="<?= Yii::getAlias('@web/image/tour/2.png 1x, @web/image/tour/2@2x.png 2x') ?>"
                  class="img-responsive"/>
         </div>
         <div class="col-md-5">
