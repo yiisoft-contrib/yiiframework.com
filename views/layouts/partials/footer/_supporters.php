85d94db74788b946c002ceac1e757a54fd660ac9
Arhell <arhell333@gmail.com>
2020-07-03 11:42:10 +0300

    fix supported links

--- views/layouts/partials/footer/_supporters.php
+++ views/layouts/partials/footer/_supporters.php
@@ -3,12 +3,12 @@
         <div>Supported by</div>
     </li>
     <li class="footerList_item">
-            <a href="https://opencollective.com/yiisoft"" aria-label="OpenCollective" target="_blank" rel="noopener
+            <a href="https://opencollective.com/yiisoft" aria-label="OpenCollective" target="_blank" rel="noopener
             noreferrer"><img
                     alt="OpenCollective" height="40" src="<?= Yii::getAlias('@web/image/collective.png') ?>"/></a>
     </li>
     <li class="footerList_item">
-        <a href="https://www.jetbrains.com/?from=yii"" aria-label="JetBrains website" target="_blank" rel="noopener
+        <a href="https://www.jetbrains.com/?from=yii" aria-label="JetBrains website" target="_blank" rel="noopener
         noreferrer"><img
                 alt="JetBrains logo" width="80" src="<?= Yii::getAlias('@web/image/jetbrains.svg') ?>"/></a>
     </li>
