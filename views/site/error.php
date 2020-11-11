89c1d45c0611b4a18843ad10b813fa28e0188340
Ihor Sychevskyi <arhell333@gmail.com>
2020-11-12 00:24:43 +0200

    update 404 page (#657)

--- views/site/error.php
+++ views/site/error.php
@@ -12,22 +12,20 @@ $this->title = $name;
 ?>
 <?= $this->render('partials/common/_heading.php', ['title' => $this->title]) ?>
 <div class="container">
-    <div class="row">
-        <div class="site-error content">
+    <div class="site-error content">
 
-            <div class="alert alert-warning">
-                <?= nl2br(Html::encode($message)) ?>
-            </div>
-            <img src="<?= Yii::getAlias('@web/image/404.jpg') ?>"
-                alt="Something is wrong: <?= Html::encode($this->title) ?>"
-                class="img-responsive" />
-            <p>
-                The above error occurred while the Web server was processing your request.
-            </p>
-            <p>
-                Please <?= Html::a('contact us', Url::to(['site/contact'])) ?> if you think this is a server error. Thank you.
-            </p>
-
+        <div class="alert alert-warning">
+            <?= nl2br(Html::encode($message)) ?>
         </div>
+        <img src="<?= Yii::getAlias('@web/image/404.jpg') ?>"
+            alt="Something is wrong: <?= Html::encode($this->title) ?>"
+            class="img-responsive" />
+        <p>
+            The above error occurred while the Web server was processing your request.
+        </p>
+        <p>
+            Please <?= Html::a('contact us', Url::to(['site/contact'])) ?> if you think this is a server error. Thank you.
+        </p>
+
     </div>
 </div>
