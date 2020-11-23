8c25f2f356619af0583b84863507d6dd0452adfb
Ihor Sychevskyi <arhell333@gmail.com>
2020-11-24 00:39:51 +0200

    update contact page (#666)

--- views/site/contact.php
+++ views/site/contact.php
@@ -10,6 +10,7 @@ use yii\captcha\Captcha;
 /* @var $model app\models\ContactForm */
 ?>
 <div class="container">
+    <div class="row">
         <div class="content">
             <div class="col-xs-12 col-sm-6">
                 <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
@@ -26,34 +27,35 @@ use yii\captcha\Captcha;
                         Thank you.
                     </p>
                     <?php $form = ActiveForm::begin() ?>
-                        <?= $form->field($model, 'name', [
-                            'inputOptions' => ['placeholder' => $model->getAttributeLabel('name')]
-                        ])->label(false) ?>
+                    <?= $form->field($model, 'name', [
+                        'inputOptions' => ['placeholder' => $model->getAttributeLabel('name')]
+                    ])->label(false) ?>
 
-                        <?= $form->field($model, 'email', [
-                            'inputOptions' => ['placeholder' => $model->getAttributeLabel('email')]
-                        ])->label(false) ?>
-
-
-                        <?= $form->field($model, 'subject', [
-                            'inputOptions' => ['placeholder' => $model->getAttributeLabel('subject')]
-                        ])->label(false) ?>
-
-                        <?= $form->field($model, 'body', [
-                            'inputOptions' => ['placeholder' => $model->getAttributeLabel('body')]
-                        ])->label(false)->textarea(['rows' => 6]) ?>
+                    <?= $form->field($model, 'email', [
+                        'inputOptions' => ['placeholder' => $model->getAttributeLabel('email')]
+                    ])->label(false) ?>
 
 
-                        <?= $form->field($model, 'verifyCode', [
-                            'inputOptions' => ['placeholder' => 'Verification Code']
-                        ])->label('Verification code: ')->widget(Captcha::class, [
-                            'template' => '{image}{input}',
-                        ]) ?>
+                    <?= $form->field($model, 'subject', [
+                        'inputOptions' => ['placeholder' => $model->getAttributeLabel('subject')]
+                    ])->label(false) ?>
 
-                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                    <?= $form->field($model, 'body', [
+                        'inputOptions' => ['placeholder' => $model->getAttributeLabel('body')]
+                    ])->label(false)->textarea(['rows' => 6]) ?>
+
+
+                    <?= $form->field($model, 'verifyCode', [
+                        'inputOptions' => ['placeholder' => 'Verification Code']
+                    ])->label('Verification code: ')->widget(Captcha::class, [
+                        'template' => '{image}{input}',
+                    ]) ?>
+
+                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
                     <?php ActiveForm::end(); ?>
                 <?php endif ?>
             </div>
             <div class="clearfix"></div>
         </div>
+    </div>
 </div>
