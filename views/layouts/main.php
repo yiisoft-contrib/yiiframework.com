de6b4d3e323e930a48e580319acb5a3f7c91aa52
Arhell <arhell333@gmail.com>
2021-01-19 01:11:12 +0200

    update favicons

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,12 +26,11 @@ $this->registerLinkTag([
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="<?= Yii::getAlias('@web/favico/apple-touch-icon.png') ?>">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= Yii::getAlias('@web/apple-touch-icon.png') ?>">
         <link rel="icon" type="image/png" sizes="32x32" href="<?= Yii::getAlias('@web/favico/favicon-32x32.png') ?>">
         <link rel="icon" type="image/png" sizes="16x16" href="<?= Yii::getAlias('@web/favico/favicon-16x16.png') ?>">
         <link rel="manifest" href="<?= Yii::getAlias('@web/favico/manifest.json') ?>">
         <link rel="mask-icon" href="<?= Yii::getAlias('@web/favico/safari-pinned-tab.svg') ?>" color="#2b5797">
-        <link rel="shortcut icon" href="<?= Yii::getAlias('@web/favico/favicon.ico') ?>">
         <meta name="msapplication-config" content="<?= Yii::getAlias('@web/favico/browserconfig.xml') ?>">
         <meta name="theme-color" content="#ffffff">
 
