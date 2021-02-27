70cbbadc8a8a0e3de16bdd7d9c64e00dfa612c4a
Arhell <arhell333@gmail.com>
2021-02-27 10:47:28 +0200

    update favicons

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,13 +26,14 @@ $this->registerLinkTag([
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-        <link rel="apple-touch-icon" sizes="180x180" href="<?= Yii::getAlias('@web/apple-touch-icon.png') ?>">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= Yii::getAlias('@web/favico/apple-touch-icon.png') ?>">
         <link rel="icon" type="image/png" sizes="32x32" href="<?= Yii::getAlias('@web/favico/favicon-32x32.png') ?>">
         <link rel="icon" type="image/png" sizes="16x16" href="<?= Yii::getAlias('@web/favico/favicon-16x16.png') ?>">
+        <link rel="shortcut icon" href="<?= Yii::getAlias('@web/favico/favicon.ico') ?>">
         <link rel="manifest" href="<?= Yii::getAlias('@web/favico/manifest.json') ?>">
-        <link rel="mask-icon" href="<?= Yii::getAlias('@web/favico/safari-pinned-tab.svg') ?>" color="#2b5797">
+        <link rel="mask-icon" href="<?= Yii::getAlias('@web/favico/safari-pinned-tab.svg') ?>" color="#1e6887">
         <meta name="msapplication-config" content="<?= Yii::getAlias('@web/favico/browserconfig.xml') ?>">
-        <meta name="theme-color" content="#ffffff">
+        <meta name="theme-color" content="#1e6887">
 
         <link href="<?= Url::to(['rss/all'], true) ?>" type="application/rss+xml" rel="alternate" title="Lives News for Yii Framework">
 
