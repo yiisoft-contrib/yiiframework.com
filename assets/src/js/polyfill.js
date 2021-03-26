42893d998adb51bf03fc8db7aadb2c989751171f
Ihor Sychevskyi <arhell333@gmail.com>
2021-03-27 00:06:24 +0200

    refactor polyfill js (#759)

--- assets/src/js/polyfill.js
+++ assets/src/js/polyfill.js
@@ -2,7 +2,7 @@
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/Trim#Polyfill
 if (!String.prototype.trim) {
-  String.prototype.trim = function () {
-    return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
-  };
+    String.prototype.trim = function () {
+        return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+    };
 }
