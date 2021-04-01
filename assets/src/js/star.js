f6cf3cd5963ca62b45b473736d31585f315f460b
Ihor Sychevskyi <arhell333@gmail.com>
2021-04-02 01:17:27 +0300

    refactor star js (#763)

--- assets/src/js/star.js
+++ assets/src/js/star.js
@@ -1,14 +1,16 @@
-$(function($) {
-    $('[data-star-url]').on('click', function() {
+$(function ($) {
+    $('[data-star-url]').on('click', function () {
         var el = $(this);
         el.addClass('fa-spin');
         $.ajax({
             url: el.data('star-url'),
             method: 'post',
             dataType: 'json',
-            success: function(data) {
+            success: function (data) {
                 el.removeClass('fa-star fa-star-o');
-                setTimeout(function() { el.removeClass('fa-spin'); }, 300);
+                setTimeout(function () {
+                    el.removeClass('fa-spin');
+                }, 300);
                 if (data['star'] == 1) {
                     el.addClass('fa-star');
                 } else {
@@ -19,7 +21,8 @@ $(function($) {
                     count.text(data['starCount']);
                 }
             },
-            error: function() {}
+            error: function () {
+            }
         });
     });
 });
