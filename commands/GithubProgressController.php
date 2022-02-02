4475b73ec97925234627b521c08db8fb8b7e4f05
Alexander Makarov <sam@rmcreative.ru>
2022-02-02 09:25:45 +0300

    Fix #983: Do not output when github-progress succeeds

--- commands/GithubProgressController.php
+++ commands/GithubProgressController.php
@@ -8,36 +8,49 @@ use Github\Exception\RuntimeException;
 use Yii;
 use yii\console\Controller;
 
-class GithubProgressController extends Controller
+final class GithubProgressController extends Controller
 {
-    const RETRY_ATTEMPTS_COUNT = 3;
-    const RETRY_DELAY = 30; // seconds
+    private const RETRY_ATTEMPTS_COUNT = 3;
+    private const RETRY_DELAY = 30; // seconds
 
     public function actionIndex()
     {
         foreach (GithubProgress::VERSIONS as $version) {
-            for ($attempt = 1; $attempt <= static::RETRY_ATTEMPTS_COUNT; $attempt++) {
-                $this->stdout("Getting data for version $version...\n");
+            $this->fetchProgress($version);
+        }
+    }
 
-                try {
-                    $data = (new GithubProgress($version, new GithubClient()))->getData();
-                    Yii::$app->cache->set("github_progress_data_$version", $data);
+    private function fetchProgress(string $version): void
+    {
+        $attempt = 0;
+        $failures = 0;
 
-                    break;
-                } catch (RuntimeException $e) {
-                    $keyText = 'This may be the result of a timeout, or it could be a GitHub bug.';
-                    if (strpos($e->getMessage(), $keyText === false)) {
-                        throw $e;
-                    }
+        while ($attempt < self::RETRY_ATTEMPTS_COUNT) {
+            try {
+                $data = (new GithubProgress($version, new GithubClient()))->getData();
+                Yii::$app->cache->set("github_progress_data_$version", $data);
 
-                    $retryDelay = static::RETRY_DELAY;
-                    $message = "Failed to get data for version $version because of timeout.\n
-                    Retrying in $retryDelay seconds...\n";
-
-                    $this->stderr($message);
-                    sleep(static::RETRY_DELAY);
+                break;
+            } catch (RuntimeException $e) {
+                $keyText = 'This may be the result of a timeout, or it could be a GitHub bug.';
+                if (strpos($e->getMessage(), $keyText) === false) {
+                    throw $e;
                 }
+
+                sleep(self::RETRY_DELAY);
+                $failures++;
             }
+
+            $attempt++;
+        }
+
+        if ($failures === self::RETRY_ATTEMPTS_COUNT) {
+            $this->stderr(sprintf(
+                "Failed to get data for version %s because of timeout.\nRetried %s times with %s seconds interval.\n",
+                $version,
+                $attempt,
+                self::RETRY_DELAY
+            ));
         }
     }
 }
