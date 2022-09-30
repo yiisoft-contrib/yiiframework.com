7f732cfdc08bb1d42bf515e78980cf0cb287d671
Marco van 't Wout <marco@tremani.nl>
2022-09-30 13:59:34 +0200

    Release Yii 1.1.26

--- config/versions.php
+++ config/versions.php
@@ -50,7 +50,7 @@ HTML
 			// - guide pdf
 			// - blog pdf
 			// - api html
-			'download-url'=>'https://github.com/yiisoft/yii/releases/download/1.1.25/yii-1.1.25.43e386',
+			'download-url'=>'https://github.com/yiisoft/yii/releases/download/1.1.26/yii-1.1.26.bda32f',
 			'min-php-version' => '5.1.0',
 			'support-until' => 'December 31, 2016 ',
 			'security-until' => 'December 31, 2023 ',
@@ -134,6 +134,7 @@ HTML
 			'2.0.0-alpha' => 'December 1, 2013',
 		],
 		'1.1' => [
+            '1.1.26' => 'September 30, 2022',
             '1.1.25' => 'December 13, 2021',
             '1.1.24' => 'June 7, 2021',
             '1.1.23' => 'December 2, 2020',
