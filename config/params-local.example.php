9ddba1519079f12d1879d3eaeb0f2a21a4c4fcbc
Alexey Rogachev <arogachev90@gmail.com>
2023-06-23 17:02:31 +0600

    Add example configs

---
+++ config/params-local.example.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    'components.db' => [
+        'class' => 'yii\db\Connection',
+        'charset' => 'utf8',
+
+        'dsn' => 'mysql:host=mariadb;dbname=yiiframeworkcom',
+        'username' => 'yiiframeworkcom',
+        'password' => 'yiiframeworkcom',
+    ],
+
+    'components.cache' => [
+        'class' => yii\redis\Cache::class,
+        'redis' => [
+            'class' => yii\redis\Connection::class,
+            'hostname' => 'redis',
+            'database' => 1,
+        ],
+    ],
+
+    'components.elasticsearch' => [
+        'class' => yii\elasticsearch\Connection::class,
+        'nodes' => [
+            ['http_address' => 'elasticsearch:9200'],
+        ],
+    ],
+
+    'authclients' => [
+        'github' => [
+            'class' => 'yii\authclient\clients\GitHub',
+            'scope' => 'user:email',
+
+            // register a new application on Github and enter client secrets here
+            'clientId' => '',
+            'clientSecret' => '',
+        ],
+    ],
+
+    'siteAbsoluteUrl' => 'http://local.yiiframework.com',
+
+    // https://apps.twitter.com/app/new
+    // After creating an app you need to fill accessToken and accessTokenSecret:
+    // Open App -> Keys and Access Tokens -> You Access Token -> Create my access token
+    'twitter.consumerKey' => '',
+    'twitter.consumerSecret' => '',
+    'twitter.accessToken' => '',
+    'twitter.accessTokenSecret' => '',
+];
