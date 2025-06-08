6a1490f5636b7b7cc323b37770028ddbceae96ed
Alexander Makarov <sam@rmcreative.ru>
2025-06-08 11:47:58 +0300

    Initial work on Sentry

--- env/prod/config/console-local.php
+++ env/prod/config/console-local.php
@@ -1,17 +1,5 @@
 <?php
 
 return [
-    'bootstrap' => [
-        'rollbar',
-    ],
-    'components' => [
-        'errorHandler' => [
-            'class' => 'baibaratsky\yii\rollbar\console\ErrorHandler',
-        ],
-        'rollbar' =>  [
-            'class' => 'baibaratsky\yii\rollbar\Rollbar',
-            'accessToken' => '',
-            'environment' => YII_ENV,
-        ],
-    ],
+
 ];
--- env/prod/config/web-local.php
+++ env/prod/config/web-local.php
@@ -1,27 +1,12 @@
 <?php
 
 return [
-    'bootstrap' => [
-        'rollbar',
-    ],
     'components' => [
         'request' => [
             'cookieValidationKey' => '',
         ],
         'errorHandler' => [
-            'class' => 'baibaratsky\yii\rollbar\web\ErrorHandler',
-            'payloadDataCallback' => function (\baibaratsky\yii\rollbar\web\ErrorHandler $errorHandler) {
-                return [
-                    'exceptionCode' => $errorHandler->exception->getCode(),
-                    'rawRequestBody' => Yii::$app->request->getRawBody(),
-                ];
-            },
             'errorAction' => 'site/error',
-        ],
-        'rollbar' =>  [
-            'class' => 'baibaratsky\yii\rollbar\Rollbar',
-            'accessToken' => '',
-            'environment' => YII_ENV,
         ],
     ],
 ];
